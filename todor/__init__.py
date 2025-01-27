e7b7d272e76cdcc7083e642d2eb98865fed847ca
Lissette Ayala <ayala.lissette@gmail.com>
2025-01-26 22:57:40 -0600

    first commit

---
+++ todor/__init__.py
@@ -0,0 +1,38 @@
+# Importando la clase Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+# Creando extension de la base de datos
+db = SQLAlchemy()
+
+# Creando función de control
+def create_app():
+
+    # Creando la variable de iniciación
+    app = Flask(__name__)
+
+    # Configuración del proyecto
+    app.config.from_mapping(
+        DEBUG = True,
+        SECRET_KEY = 'dev_esit',
+        SQLALCHEMY_DATABASE_URI = "sqlite:///todolist.db"
+    )
+    # Iniciando conexion
+    db.init_app(app)
+    # Regitrando Blueprint
+    from.import todo
+    app.register_blueprint(todo.bp)
+
+    from.import auth
+    app.register_blueprint(auth.bp)
+
+
+    # Definiendo rutas
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    # Creacion de tablas a partir de los modelos
+    
+    with app.app
+    return app
